c0dcb4e09f059d444bb892943c2d5e31c994a63a
Colton Berry <coltonsberry@gmail.com>
2021-10-08 08:30:40 -0400

    Add GPU miner link to mine tab

--- src/pages/Mine/Mine.tsx
+++ src/pages/Mine/Mine.tsx
@@ -28,16 +28,18 @@ function secondsToDhms(d: number) {
   } else if (Number.isNaN(days)) {
     return "invalid time";
   }
-  
+
   return days.toString() + " days";
 }
 
 function secondsToHms(d: number) {
   const h = Math.floor(d / 3600);
-  const m = Math.floor(d % 3600 / 60);
-  const s = Math.floor(d % 3600 % 60);
+  const m = Math.floor((d % 3600) / 60);
+  const s = Math.floor((d % 3600) % 60);
 
-  return ('0' + h).slice(-2) + ":" + ('0' + m).slice(-2) + ":" + ('0' + s).slice(-2);
+  return (
+    ("0" + h).slice(-2) + ":" + ("0" + m).slice(-2) + ":" + ("0" + s).slice(-2)
+  );
 }
 
 function formatPercentage(p: number) {
@@ -50,21 +52,37 @@ function formatPercentage(p: number) {
 
 export const Mine = () => {
   HackilyRewriteHistory({ title: "mine" });
-  const [individualMiningRate, setIndividualMiningRate] = useState<number>(INDIVIDUAL_MINING_RATE);
-  const [networkMiningRate, setNetworkMiningRate] = useState<number>(100 * INDIVIDUAL_MINING_RATE);
-  const displayedIndividualMiningRate = Number.isNaN(individualMiningRate) ? "" : individualMiningRate;
-  const displayedNetworkMiningRate = Number.isNaN(networkMiningRate) ? "" : networkMiningRate;
+  const [individualMiningRate, setIndividualMiningRate] = useState<number>(
+    INDIVIDUAL_MINING_RATE
+  );
+  const [networkMiningRate, setNetworkMiningRate] = useState<number>(
+    100 * INDIVIDUAL_MINING_RATE
+  );
+  const displayedIndividualMiningRate = Number.isNaN(individualMiningRate)
+    ? ""
+    : individualMiningRate;
+  const displayedNetworkMiningRate = Number.isNaN(networkMiningRate)
+    ? ""
+    : networkMiningRate;
 
+  const winPercentage =
+    networkMiningRate <= 0 || individualMiningRate > networkMiningRate
+      ? 1
+      : individualMiningRate / networkMiningRate;
 
-  const winPercentage = (networkMiningRate <= 0 || individualMiningRate > networkMiningRate) ? 1 : individualMiningRate / networkMiningRate;
+  const onIndividualMiningRateChange = useCallback(
+    (e: React.FormEvent<HTMLInputElement>) => {
+      setIndividualMiningRate(parseNumber(e));
+    },
+    [setIndividualMiningRate]
+  );
 
-  const onIndividualMiningRateChange = useCallback((e: React.FormEvent<HTMLInputElement>) => {
-    setIndividualMiningRate(parseNumber(e));
-  }, [setIndividualMiningRate]);
-
-  const onNetworkMiningRateChange = useCallback((e: React.FormEvent<HTMLInputElement>) => {
-    setNetworkMiningRate(parseNumber(e));
-  }, [setNetworkMiningRate]);
+  const onNetworkMiningRateChange = useCallback(
+    (e: React.FormEvent<HTMLInputElement>) => {
+      setNetworkMiningRate(parseNumber(e));
+    },
+    [setNetworkMiningRate]
+  );
 
   const individualTimeToMint = React.useMemo(() => {
     return getTimeToMint(individualMiningRate);
@@ -77,37 +95,77 @@ export const Mine = () => {
   return (
     <div className={styles.container}>
       <div className={styles.inner}>
-        mining is no longer feasible through the web browser. a GPU miner is under
-        development and updates will be posted via twitter and discord.
+        the mpunks GPU miner can be found here:
       </div>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/mineablepunks/mpunks-miner-bundle-windows-x64"
+      >
+        mpunks-miner-bundle-windows-x64
+      </a>
       <br />
       <div className={classNames(styles.inner, styles.content)}>
         <b>When should I mine?</b>
-        <p className={styles.marginBottom}>always! no, that's not really true. we have put together some info to help you decide if it makes sense for you to mine.</p>
+        <p className={styles.marginBottom}>
+          always! no, that's not really true. we have put together some info to
+          help you decide if it makes sense for you to mine.
+        </p>
         <Divider className={styles.marginBottom} />
         <div className={styles.inputs}>
-        <Fieldset className={styles.input} legend={"your mining rate (MH/s)"}>
-          <Input type={"number"} value={displayedIndividualMiningRate} onChange={onIndividualMiningRateChange} />
-        </Fieldset>
-        <Fieldset className={styles.input} legend={"network mining rate (MH/s)"}>
-          <Input type={"number"} value={displayedNetworkMiningRate} onChange={onNetworkMiningRateChange} />
-        </Fieldset>
+          <Fieldset className={styles.input} legend={"your mining rate (MH/s)"}>
+            <Input
+              type={"number"}
+              value={displayedIndividualMiningRate}
+              onChange={onIndividualMiningRateChange}
+            />
+          </Fieldset>
+          <Fieldset
+            className={styles.input}
+            legend={"network mining rate (MH/s)"}
+          >
+            <Input
+              type={"number"}
+              value={displayedNetworkMiningRate}
+              onChange={onNetworkMiningRateChange}
+            />
+          </Fieldset>
         </div>
         <div className={styles.outputs}>
-          <Output className={styles.output} label={"current difficulty"} value={CURRENT_DIFFICULTY} />
-          <Output className={styles.output} label={"time for you to mint (ddd:hh:mm:ss)"} value={secondsToDhms(individualTimeToMint)} />
-          <Output className={styles.output} label={"time for network to mint (ddd:hh:mm:ss)"} value={secondsToDhms(networkTimeToMint)} />
-          <Output className={styles.output} label={"chances of you winning"} value={formatPercentage(winPercentage)} />
+          <Output
+            className={styles.output}
+            label={"current difficulty"}
+            value={CURRENT_DIFFICULTY}
+          />
+          <Output
+            className={styles.output}
+            label={"time for you to mint (ddd:hh:mm:ss)"}
+            value={secondsToDhms(individualTimeToMint)}
+          />
+          <Output
+            className={styles.output}
+            label={"time for network to mint (ddd:hh:mm:ss)"}
+            value={secondsToDhms(networkTimeToMint)}
+          />
+          <Output
+            className={styles.output}
+            label={"chances of you winning"}
+            value={formatPercentage(winPercentage)}
+          />
         </div>
       </div>
     </div>
   );
 };
 
-const Output = React.memo((props: {className?: string, label: string, value: string}) => {
-  return (
-    <div className={props.className}>
-      <p><b>{props.label}:</b> {props.value}</p>
-    </div>
-  );
-});
+const Output = React.memo(
+  (props: { className?: string; label: string; value: string }) => {
+    return (
+      <div className={props.className}>
+        <p>
+          <b>{props.label}:</b> {props.value}
+        </p>
+      </div>
+    );
+  }
+);
